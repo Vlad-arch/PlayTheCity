d19d0daad69528f6b53758bb8c5b09eab93d1f94
frasac <sacconifranc@gmail.com>
2026-03-28 12:14:10 +0100

    first commit

---
+++ src/main/java/it/siali/playthecity/PlaythecityApplication.java
@@ -0,0 +1,13 @@
+package it.siali.playthecity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaythecityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaythecityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/siali/playthecity/PlaythecityApplicationTests.java
@@ -0,0 +1,13 @@
+package it.siali.playthecity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaythecityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
